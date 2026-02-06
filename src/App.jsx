0af124f481de71789163676f77c2553647c950b7
Ethan Hayes <yokasama.ss@gmail.com>
2026-02-05 23:14:17 -0500

    完成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
+import Question from "./components/question";
+import { useState } from "react";
+import starIcon from "./assets/images/icon-star.svg";
 
 function App() {
-    return <></>;
+    return (
+        <div className="FAQ-contanier">
+            <div className="FAQ-header">
+                <div className="FAQ-header__star">
+                    <img src={starIcon} alt="Star Icon" />
+                </div>
+                <h1>FAQs</h1>
+            </div>
+            <Question
+                question="What is Frontend Mentor, and how will it help me?"
+                answer="Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building."
+            />
+            <hr className="decoration-line" />
+            <Question
+                question="Is Frontend Mentor free?"
+                answer="Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+  option providing access to a range of projects suitable for all skill levels."
+            />
+            <hr className="decoration-line" />
+            <Question
+                question="Can I use Frontend Mentor projects in my portfolio?"
+                answer="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+  way to showcase your skills to potential employers!"
+            />
+            <hr className="decoration-line" />
+            <Question
+                question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
+                answer="The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+  channel where you can ask questions and seek support from other community members."
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Question.jsx
@@ -0,0 +1,24 @@
+import plusIcon from "../assets/images/icon-plus.svg";
+import minusIcon from "../assets/images/icon-minus.svg";
+
+import { useState } from "react";
+
+export default function Question({ question, answer }) {
+    const mins = <img src={minusIcon} alt="plusIcon" className="Icon" />;
+    const plus = <img src={plusIcon} alt="plusIcon" className="Icon" />;
+
+    const [isHidden, setIsHidden] = useState(true);
+    const handleClick = () => {
+        setIsHidden((pre) => !pre);
+    };
+
+    return (
+        <div className="FAQ-text">
+            <div className="FAQ-text__title">
+                <h2 className="FAQ__question">{question}</h2>
+                <button onClick={handleClick}>{isHidden ? plus : mins}</button>
+            </div>
+            {isHidden ? null : <p>{answer}</p>}
+        </div>
+    );
+}
